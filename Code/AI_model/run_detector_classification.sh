467233b4507556f73b06060d075e66c5b5c3aef1
Ofir Levy <levyofi@gmail.com>
2022-12-31 21:06:30 +0200

    Update run_detector_classification.sh

--- Code/AI_model/run_detector_classification.sh
+++ Code/AI_model/run_detector_classification.sh
@@ -6,5 +6,5 @@ N=$1
 
 # shellcheck disable=SC2051
 for ((i=1;i<=$N;i++)); do
-    gnome-terminal --tab --title="lizard_detection$i" -- /bin/bash -c "export CUDA_VISIBLE_DEVICES=$GPU; python detect_fast_rcnn_and_classification_for_winter_paper.py; exec bash;"
+    gnome-terminal --tab --title="lizard_detection$i" -- /bin/bash -c "export CUDA_VISIBLE_DEVICES=$GPU; python detect_and_classify.py; exec bash;"
 done
